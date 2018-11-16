c10a51c3990d95d0b0c19e01c1e23741a97c0e38
lagooj <lagooj@m-projects.org>
2018-11-16 16:50:37 +0100

    use std::process and add verbose flags

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ extern crate clap;
 extern crate redis;
 
 use clap::{Arg, App};
-
+use std::process;
 
 fn main() {
 
     let cli_matches = App::new("check_redis_simple")
-        .author("M3")
+        .author("lagooj")
         .version("0.1")
-        .about("Check redis status and memory usage, should be runned by icinga")
+        .about("Check redis status and memory usage, should be runned by icinga, nagios")
         .arg(Arg::with_name("WARNING")
              .short("w")
              .long("warning")
@@ -26,13 +26,17 @@ fn main() {
              .short("H")
              .long("hostname")
              .help("Set hostname and port ex: localhost:6379")
-             .takes_value(true))
+             .takes_value(true)
+             .required(true))
+        .arg(Arg::with_name("v")
+             .short("v")
+             .help("Set verbose mode")
+             .multiple(false))
         .get_matches();
 
     let warning_ts :u8 = cli_matches.value_of("WARNING").unwrap().parse::<u8>().unwrap();
     let critical_ts :u8 = cli_matches.value_of("CRITICAL").unwrap().parse::<u8>().unwrap();
     let hostname = cli_matches.value_of("HOSTNAME:PORT").unwrap();
-
     let percent = match get_memory_values(hostname) {
         Err(e) => panic!(e),
         Ok(v) => compute_percent(v),
@@ -41,9 +45,11 @@ fn main() {
     match percent {
         _ if percent > critical_ts => println!("Critical triggered"),
         _ if percent > warning_ts => println!("Warning triggered"),
-        _ => println!("Ok"),
+        _ => process::exit(0)
     };
+    process::exit(0);
 }
+
 pub fn get_memory_values(hostname: &str) -> Result<Vec<usize>,redis::RedisError> {
 
     let redis_host= format!("redis://{}", hostname);
@@ -64,7 +70,6 @@ fn compute_percent(mem: Vec<usize>) -> u8 {
     let used = mem[1];
     println!("{} - {}", used as u32, max  as u32);
     println!("{}", ((used as f64 / max as f64) * 100.00));
-
     if max == 0  || used == 0 {
         return 100;
     }
